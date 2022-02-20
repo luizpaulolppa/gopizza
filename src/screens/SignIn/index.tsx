315fd5a1a54b621a051faac93004f356cc1b2af0
Luiz Paulo Pilegi de Almeida <luiz.pilegi@condoconta.com.br>
2022-02-20 17:09:11 -0300

    creating new button component

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 import { Container } from "./styles";
 
@@ -13,11 +14,9 @@ export function SignIn() {
         autoCapitalize="none"
       />
 
-      <Input
-        placeholder="Senha"
-        type="secondary"
-        secureTextEntry
-      />
+      <Input placeholder="Senha" type="secondary" secureTextEntry />
+
+      <Button title="Entrar" type="primary" />
     </Container>
   );
 }
